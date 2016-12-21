10a83254f26ce243f59f944c17e0d0530922592a
Lingerhk <lingerhk@gmail.com>
2016-12-20 21:40:33 -0500

    update fs_agent

---
+++ trunk/fs_agent/fsa_cfg.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+
+
+from yg_base_cfg import *
+
+
+class EnvEnum:
+    T_DEV       =   "dev"
+    T_ONLINE    =   "online"
+
+    
+CUR_ENV         =   EnvEnum.T_DEV
+
+if CUR_ENV ==  EnvEnum.T_DEV:
+    
+    BaseConf.IS_CTR_LOG     =   True
+    BaseConf.LOG_LEVEL      =   1
+
+    SERVER_IP               =   "127.0.0.1"
+    SERVER_PORT             =   8000
+   
+
+else:
+    BaseConf.IS_CTR_LOG     =   True
+    BaseConf.LOG_LEVEL      =   1
+
+    SERVER_IP               =   "127.0.0.1"
+    SERVER_PORT             =   8000    
+  
+
+
+# with local cfg, so do not modify the file when test in local env
+if os.path.exists(os.path.join(os.path.dirname(__file__), 'fsa_local_cfg.py')):
+    from fsa_local_cfg import *
+    
+    
+    
+    
+    
+    
