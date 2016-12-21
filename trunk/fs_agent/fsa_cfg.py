2a8a1d2e4ff2a5a35181dc38961411455ccbceaa
Lingerhk <lingerhk@gmail.com>
2016-12-20 21:49:04 -0500

    update fas_cfg

--- trunk/fs_agent/fsa_cfg.py
+++ trunk/fs_agent/fsa_cfg.py
@@ -1,16 +1,19 @@
 # -*- coding: UTF-8 -*-
 
+# project: fshell
+# author: s0nnet
+# time: 2016-12-10
+# desc: agent main conf
 
 
-from yg_base_cfg import *
-
+from fs_base_cfg import *
 
 class EnvEnum:
     T_DEV       =   "dev"
     T_ONLINE    =   "online"
-
     
 CUR_ENV         =   EnvEnum.T_DEV
+
 
 if CUR_ENV ==  EnvEnum.T_DEV:
     
@@ -20,7 +23,6 @@ if CUR_ENV ==  EnvEnum.T_DEV:
     SERVER_IP               =   "127.0.0.1"
     SERVER_PORT             =   8000
    
-
 else:
     BaseConf.IS_CTR_LOG     =   True
     BaseConf.LOG_LEVEL      =   1
@@ -28,7 +30,6 @@ else:
     SERVER_IP               =   "127.0.0.1"
     SERVER_PORT             =   8000    
   
-
 
 # with local cfg, so do not modify the file when test in local env
 if os.path.exists(os.path.join(os.path.dirname(__file__), 'fsa_local_cfg.py')):
