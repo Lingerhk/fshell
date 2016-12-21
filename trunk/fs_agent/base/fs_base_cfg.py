6bd5f7d321b3367341606da0a67f6f19dfc15e40
Lingerhk <lingerhk@gmail.com>
2016-12-20 21:46:26 -0500

    add base module

---
+++ trunk/fs_agent/base/fs_base_cfg.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# project: fshell 
+# author: s0nnet
+# time: 2016-12-10
+# desc: agent base conf
+
+
+'''
+LOG_LEVEL
+    DEBUG   =   1
+    INFO    =   2
+    WARINING =  3
+    ERR     =   4
+    ALERT   =   5
+    CLOSE   =   10
+'''
+class BaseConf:
+
+    LOG_LEVEL       =   1
+    LOG_DIR         =   "../../log/"
+    LOG_PREFIX      =   ""
+    IS_CTR_LOG      =   True
